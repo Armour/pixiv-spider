225eed1606d6e8090e3e237d6cd20b0802f1c7c9
Chong Guo <cguo@athinkingape.com>
2017-03-14 23:42:31 -0700

    updated spider

---
+++ config.py
@@ -0,0 +1,12 @@
+class RankingType():
+    DAY = 'day',
+    WEEK = 'week',
+    MONTH = 'month',
+    DAY_MALE = 'day_male',
+    DAY_FEMALE = 'day_female',
+    WEEK_ORIGIONAL = 'week_origional',
+    WEEK_ROOKIE = 'week_rookie',
+    DAY_MANGA = 'day_manga'
+
+
+DEFAULT_DOWNLOAD_COUNT = 50
--- spider.py
+++ spider.py
@@ -1,20 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Author: Chong Guo
-# @Date:   2017-03-12 23:59:59
+# @Date:   2017-03-12 23:59:09
 
-import requests
+import os
 
-login_url = r'https://www.pixiv.net/login.php'
-data = {
-    'mode': 'login',
-    'return_to': '/',
-    'pixiv_id': '493377211',
-    'pass': 'qw123456',
-    'skip': '1'
-}
+from pixivpy3 import AppPixivAPI
+from config import RankingType, DEFAULT_DOWNLOAD_COUNT
+from datetime import datetime
 
-s = requests.Session()
-res = s.post(login_url, data=data)
-print(res.status_code)
-print(res.text)
+
+class PixivSpider:
+    def __init__(self):
+        """
+        Init PixivSpider
+        """
+        self.api = AppPixivAPI()
+        self.directory = "download"
+
+    def create_download_folder(self):
+        """
+        Setup image download directory
+        """
+        if not os.path.exists(self.directory):
+            os.makedirs(self.directory)
+
+    def get_top_ranking_illusts(self,
+                                count=DEFAULT_DOWNLOAD_COUNT,
+                                ranking_type=RankingType.DAY,
+                                date=datetime.today().strftime("%Y-%m-%d")):
+        """
+        Download top ranking illusts
+        :count: the number of illusts that we want to download
+        :ranking_type: ranking type
+        """
+        json_result = self.api.illust_ranking(ranking_type, date=date)
+        count = min(count, len(json_result.illusts))
+
+        for illust in json_result.illusts[:count]:
+            image_url = illust.meta_single_page.get('original_image_url',
+                                                    illust.image_urls.large)
+            print("Found %s: %s" % (illust.title, image_url))
+            url_basename = os.path.basename(image_url)
+            extension = os.path.splitext(url_basename)[1]
+            name = "illust_id_%d_%s%s" % (illust.id, illust.title, extension)
+            if not os.path.exists(self.directory + '/' + name):
+                print("Download %s: %s" % (illust.title, image_url))
+                self.api.download(image_url, path=self.directory, name=name)
+            else:
+                print("Already downloaded: %s: %s" % (illust.title, image_url))
+
+
+if __name__ == '__main__':
+    spider = PixivSpider()
+    spider.create_download_folder()
+    spider.get_top_ranking_illusts()
