16a23a8f8ac81b58ae3409c64a6638e44b658894
Chong Guo <cguo@athinkingape.com>
2017-03-19 17:05:32 -0700

    code reuse

--- spider.py
+++ spider.py
@@ -7,9 +7,19 @@ import os
 import json
 
 from pixivpy3 import AppPixivAPI, PixivError
-from config import ContentType, RankingType, DEFAULT_DOWNLOAD_COUNT_IN_TOTAL, DEFAULT_DOWNLOAD_COUNT_EACH_USER
+from config import (
+    ContentType,
+    RankingType,
+    DEFAULT_DOWNLOAD_EACH_USER_COUNT,
+    DEFAULT_DOWNLOAD_RECOMMENDED_COUNT,
+    DEFAULT_DOWNLOAD_TOP_RANKING_COUNT
+)
 from datetime import datetime
-from sets import Set
+
+try:
+    from sets import Set
+except ImportError:
+    Set = set
 
 
 class PixivSpider:
@@ -29,7 +39,6 @@ class PixivSpider:
                 self.count = len(self.data['illusts'])
                 print("Load existing info.json file")
                 print("Existed illusts count: %d" % self.count)
-        self.file = open('info.json', 'w')
         self.illusts_names = Set()
         for illust in self.data['illusts']:
             self.illusts_names.add(illust['name'])
@@ -38,8 +47,8 @@ class PixivSpider:
         """
         Stop spider and print logs
         """
-        json.dump(self.data, self.file, indent=2)
-        self.file.close()
+        with open('info.json', 'w') as f:
+            json.dump(self.data, f, indent=2)
         print("Finish! Total downloaded illusts number: %d" % self.count)
 
     def create_download_folder(self):
@@ -80,8 +89,17 @@ class PixivSpider:
             else:
                 print(u"✨  Already download: %s: %s" % (illust.title, image_url))
 
+    def get_user_ids_from_illusts(self, illusts):
+        """
+        Get user ids by illusts
+        """
+        user_ids = []
+        for illust in illusts:
+            user_ids.append(illust.user.id)
+        return user_ids
+
     def get_top_ranking_illusts(self,
-                                count=DEFAULT_DOWNLOAD_COUNT_IN_TOTAL,
+                                count=DEFAULT_DOWNLOAD_TOP_RANKING_COUNT,
                                 ranking_type=RankingType.DAY,
                                 date=datetime.today().strftime("%Y-%m-%d"),
                                 download=False):
@@ -93,24 +111,11 @@ class PixivSpider:
         :download: download flag
         """
         json_result = self.api.illust_ranking(ranking_type, date=date)
-        illusts = json_result.illusts
-        while len(json_result) != 0 and len(illusts) < count:
-            next_qs = self.api.parse_qs(json_result.next_url)
-            if next_qs is None:
-                break
-            try:
-                json_result = self.api.illust_ranking(**next_qs)
-            except TypeError:
-                break
-            illusts += json_result.illusts
-
-        if download:
-            count = min(count, len(illusts))
-            self.download_illusts(illusts=illusts[:count])
+        illusts = self.get_illusts_from_all_pages(json_result, json_result.illusts, count, download)
         return illusts[:count]
 
     def get_recommended_illusts(self,
-                                count=DEFAULT_DOWNLOAD_COUNT_IN_TOTAL,
+                                count=DEFAULT_DOWNLOAD_RECOMMENDED_COUNT,
                                 content_type=ContentType.ILLUST,
                                 download=False):
         """
@@ -120,7 +125,28 @@ class PixivSpider:
         :download: download flag
         """
         json_result = self.api.illust_recommended(content_type)
-        illusts = json_result.illusts
+        illusts = self.get_illusts_from_all_pages(json_result, json_result.illusts, count, download)
+        return illusts[:count]
+
+    def get_illusts_by_user_ids(self,
+                                user_ids,
+                                count=DEFAULT_DOWNLOAD_EACH_USER_COUNT,
+                                content_type=ContentType.ILLUST,
+                                download=False):
+        """
+        Get illusts by user id
+        """
+        ret = {}
+        for user_id in user_ids:
+            json_result = self.api.user_illusts(user_id=user_id, type=content_type)
+            illusts = self.get_illusts_from_all_pages(json_result, json_result.illusts, count, download)
+            ret[user_id] = illusts[:count]
+        return ret
+
+    def get_illusts_from_all_pages(self, json_result, illusts, count, download=False):
+        """
+        Get illusts from all pages
+        """
         while len(json_result) != 0 and len(illusts) < count:
             next_qs = self.api.parse_qs(json_result.next_url)
             if next_qs is None:
@@ -134,51 +160,15 @@ class PixivSpider:
         if download:
             count = min(count, len(illusts))
             self.download_illusts(illusts=illusts[:count])
-        return illusts[:count]
 
-    def get_user_ids_from_illusts(self, illusts):
-        """
-        Get user ids by illusts
-        """
-        user_ids = []
-        for illust in illusts:
-            user_ids.append(illust.user.id)
-        return user_ids
-
-    def get_illusts_by_user_ids(self,
-                                user_ids,
-                                count=DEFAULT_DOWNLOAD_COUNT_EACH_USER,
-                                content_type=ContentType.ILLUST,
-                                download=False):
-        """
-        Get illusts by user id
-        """
-        ret = {}
-        for user_id in user_ids:
-            json_result = self.api.user_illusts(user_id=user_id, type=content_type)
-            illusts = json_result.illusts
-            while len(json_result) != 0 and len(illusts) < count:
-                next_qs = self.api.parse_qs(json_result.next_url)
-                if next_qs is None:
-                    break
-                try:
-                    json_result = self.api.illust_ranking(**next_qs)
-                except TypeError:
-                    break
-                illusts += json_result.illusts
-
-            if download:
-                count = min(count, len(illusts))
-                self.download_illusts(illusts=illusts[:count])
-            ret[user_id] = illusts[:count]
-        return ret
+        return illusts
 
 
 if __name__ == '__main__':
     spider = PixivSpider()
     spider.create_download_folder()
-    top_ranking_illusts = spider.get_top_ranking_illusts(download=True, count=20)
-    recommended_illusts = spider.get_recommended_illusts(download=True, count=20)
+    top_ranking_illusts = spider.get_top_ranking_illusts(download=True, count=100)
+    recommended_illusts = spider.get_recommended_illusts(download=True, count=100)
     user_ids = spider.get_user_ids_from_illusts(top_ranking_illusts + recommended_illusts)
-    spider.get_illusts_by_user_ids(user_ids=user_ids, download=True, count=20)
+    spider.get_illusts_by_user_ids(user_ids=user_ids, download=True, count=100)
     spider.exit()
